c5a6a8c46b5c0362f401f53e763859fcf587c3f1
alexbalak21 <alex.balak@outlook.com>
2026-05-19 23:52:00 +0200

    update

--- Library/src/App.java
+++ Library/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        User user1 = new User(1, "Alice");
+        User user2 = new User(2, "Bob");
+        
+
     }
 }
