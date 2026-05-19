1c02bc6be780b28abb5587f39d50dc9976ed2d9f
alexbalak21 <alex.balak@outlook.com>
2026-05-19 18:37:16 +0200

    Creating library exercice

---
+++ Library/src/Book.java
@@ -0,0 +1,31 @@
+public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String id, String title, String author, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+}
+
